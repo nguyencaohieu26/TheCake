089b79ebaf5564f110e490ca4675159c8e10f392
hieunguyen26 <nguyencaohieu26@gmail.com>
2021-07-02 01:26:43 +0700

    index.html lan1

--- assets/Js/home.js
+++ assets/Js/home.js
@@ -1,3 +1,29 @@
+// scroll function
+const header__topElement = document.querySelector(".header");
+window.onscroll = function() {myFunction()};
+  function myFunction() {
+      var header__scroll = document.querySelector(".header__scroll");
+      if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        header__scroll.classList.add("active-color")
+        header__topElement.style.padding = "0"     
+      } else {
+        header__scroll.classList.remove("active-color");
+        header__topElement.style.padding = "0 1.5rem"          
+      }
+}
+// smooth scroll
+$(document).ready(function () {
+  $("a").click(function (event) {
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top,
+      },
+      1100,
+    );
+    event.preventDefault();
+  });
+});
+
 // Open mobile nav
 const btn_OpenNav = document.querySelector(".menu__toggle");
 const mobileNav = document.querySelector(".menu-mobile__nav");
@@ -35,6 +61,7 @@ btn__LoginChose.forEach(function(item,index){
     choseActivelogin(index,btn__LoginChose)
   })
 })
+// ------------------------------------------
 // Open login form
 const FormLoginElement = document.querySelector(".login__form");
 const openFormLogin    = document.querySelector(".login__icon");
@@ -45,6 +72,7 @@ openFormLogin.addEventListener("click",function(){
 closeFormLogin.addEventListener("click",function(){
   FormLoginElement.classList.remove("active");
 })
+// ---------------------------------
 // Open search
 const SearchElement = document.querySelector(".seach-container");
 const btn_OpenSearch = document.querySelector(".search__icon");
@@ -54,4 +82,72 @@ btn_OpenSearch.addEventListener("click",function(){
 })
 closeSearch.addEventListener("click",function(){
   SearchElement.classList.remove("active");
+})
+// ---------------------------------
+const ourMenulist  = document.querySelectorAll(".our-menu-cake-list");
+const btn_OpenMenu = document.querySelectorAll(".our-menu-btn-chose");
+btn_OpenMenu.forEach(function(item,index){
+  item.addEventListener("click",function(e){
+    var text = e.currentTarget.innerText;
+    ourMenulist.forEach(function(item){
+      var textcheck = item.getAttribute("data-category");
+      if(text == textcheck){
+        item.classList.add("active");
+        return;
+      }
+      item.classList.remove("active");
+    })
+    choseActivelogin(index,btn_OpenMenu);
+  })
+})
+// ------------------------------------------------------
+const cakeItemImg  = document.querySelector(".cake-inner-img img");
+const cakeDetailName     = document.querySelector(".item-cake-inner-name");
+const cakeDetailCode = document.querySelector(".item-cake-inner-code");
+const cakeDetailType = document.querySelector(".cake-inner-type-item");
+const cakeDetailPrice = document.querySelector(".item-cake-inner-price");
+//---------------------
+const detailItemCake  = document.querySelector(".detail-item-cake");
+const open_DetailCake = document.querySelectorAll(".btn-detail-feature");
+const close_DetailCake = document.querySelector(".btn-cancel-checkout");
+// function fill detail
+function fillDetailItem(img,name,code,type,price){
+  cakeItemImg.src = img;
+  cakeDetailName.innerText = name;
+  cakeDetailCode.innerText = code;
+  cakeDetailType.innerText =type;
+  cakeDetailPrice.innerText = price;
+}
+open_DetailCake.forEach(function(item){
+  item.addEventListener("click",function(e){
+    var parentElement = e.currentTarget.parentElement.parentElement.parentElement;
+    var type = parentElement.parentElement.getAttribute("data-category");  
+    var img = parentElement.querySelector("img").src;
+    var name = parentElement.querySelector(".menu-cake-item__name").innerText;
+    var price = parentElement.querySelector(".price-cake").innerText;
+    var itemcode = parentElement.querySelector(".code-item-cake").innerText;
+    fillDetailItem(img,name,itemcode,type,price);
+    detailItemCake.classList.add("active");
+  })
+})
+close_DetailCake.addEventListener("click",function(){
+  detailItemCake.classList.remove("active");
+})
+// 
+const btn__ExpandImg = document.querySelectorAll(".btn-expand-feature");
+const btn__CloseExpand = document.querySelector(".close__larger-item-img")
+const largerImgElement = document.querySelector(".larger-img-tiem");
+const largerImage      = document.querySelector(".larger-img-item-details img");
+console.log(largerImage)
+btn__ExpandImg.forEach(function(button){
+  button.addEventListener("click",function(e){
+    var target = e.currentTarget.parentElement.parentElement;
+    console.log(target.querySelector("img").src)
+    largerImage.src = target.querySelector("img").src;
+    largerImgElement.classList.add("active");
+  })
+})
+btn__CloseExpand.addEventListener("click",function(){
+  largerImgElement.classList.remove("active");
+
 })
